04e02285568f111549476c2352986c26d0cdd320
ipengyo <ipengyo@qq.com>
2015-01-08 09:46:14 +0800

    hibernat工具类

---
+++ dao/src/main/java/org/dao/ICityDao.java
@@ -0,0 +1,8 @@
+package org.dao;
+
+import org.entity.City;
+import org.hibernateUtil.IBaseDao;
+
+public interface ICityDao extends IBaseDao<City> {
+	
+}
---
+++ dao/src/main/java/org/dao/impl/CityDaoImpl.java
@@ -0,0 +1,16 @@
+package org.dao.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.dao.ICityDao;
+import org.entity.City;
+import org.hibernateUtil.AbstractBaseDao;
+import org.springframework.dao.DataAccessException;
+
+public class CityDaoImpl extends AbstractBaseDao<City> implements ICityDao {
+
+
+
+
+}
