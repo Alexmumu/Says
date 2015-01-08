de026f881bea0013996c3a466a9fd80b1ded11a5
ipengyo <ipengyo@qq.com>
2015-01-08 21:44:58 +0800

    删减了一个子模块，然后在dao模块中 添加了一个测试的范例，以及配置相关的Applicaitionxml的配置文件 方便大家预先测试一下。

--- dao/src/main/java/org/dao/impl/CityDaoImpl.java
+++ dao/src/main/java/org/dao/impl/CityDaoImpl.java
@@ -1,13 +1,11 @@
 package org.dao.impl;
 
-import java.io.Serializable;
-import java.util.List;
 
 import org.dao.ICityDao;
 import org.entity.City;
 import org.hibernateUtil.AbstractBaseDao;
-import org.springframework.dao.DataAccessException;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public class CityDaoImpl extends AbstractBaseDao<City> implements ICityDao {
 
 
---
+++ dao/src/test/java/org/dao/CityDaoImplTest.java
@@ -0,0 +1,52 @@
+package org.dao;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CityDaoImplTest 
+    extends TestCase
+{	
+	private ApplicationContext act;
+	private ICityDao citydao;
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CityDaoImplTest( String testName )
+    {
+        super( testName );
+        act=new ClassPathXmlApplicationContext("applicationContext-dao.xml");
+    	 citydao=(ICityDao) act.getBean("cityDaoImpl");
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CityDaoImplTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {	
+        assertTrue( true );
+    }
+    public void testCountAll(){
+    	Boolean istest=false;
+    	if(citydao.countAll()>0){
+    		istest=true;
+    	}
+    	assertTrue(istest);
+    }
+}
