f0214f7e7fe72714722cbeaf800ad56a4a7e2bda
ipengyo <ipengyo@qq.com>
2015-01-12 21:43:21 +0800

    增加实体对象

--- service/src/main/java/org/vo/ContentData.java
+++ service/src/main/java/org/vo/ContentData.java
@@ -8,7 +8,7 @@ import org.entity.SaysComments;
  * 展现动态的时候 可以直接用这个
  * 用于封装 
  * 带内容的数据 如 说说 日志  相片   
- * 加上  阅读数 转发转载数  评论数 
+ * 加上  阅读数 转发转载数  评论数  点赞数
  * @author ipengyo
  *
  * @param <T> 说说 日志 相片 这种数据对象
@@ -19,7 +19,7 @@ public class ContentData<T> {
 	 */
 	private T data ;
 	/**
-	 * 阅读的数目
+	 * 浏览的数目
 	 */
 	private int yuedunum;
 	/**
@@ -27,7 +27,43 @@ public class ContentData<T> {
 	 */
 	private int zhuanfanum;
 	/**
+	 * 点赞数
+	 */
+	private int dianzannum;
+	/**
 	 * 表示这个对象的类型  如 1是说说 2日志 3相册
 	 */
 	private int datatype;
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public int getYuedunum() {
+		return yuedunum;
+	}
+	public void setYuedunum(int yuedunum) {
+		this.yuedunum = yuedunum;
+	}
+	public int getZhuanfanum() {
+		return zhuanfanum;
+	}
+	public void setZhuanfanum(int zhuanfanum) {
+		this.zhuanfanum = zhuanfanum;
+	}
+	public int getDianzannum() {
+		return dianzannum;
+	}
+	public void setDianzannum(int dianzannum) {
+		this.dianzannum = dianzannum;
+	}
+	public int getDatatype() {
+		return datatype;
+	}
+	public void setDatatype(int datatype) {
+		this.datatype = datatype;
+	}
+	
+	
 }
